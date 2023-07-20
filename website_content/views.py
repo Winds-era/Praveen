e25c5c741f82fa711981d586a57d94f16ddc561c
praveenasg <praveenasg98@gmail.com>
2023-07-20 12:26:52 -0400

    automatic course assign to author

--- website_content/views.py
+++ website_content/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from course.models import Catogaries, Course
 from course.forms import CategoryForm, CourseForm
+import re
+
 
 # Create your views here.
 def home_view(request):
@@ -26,6 +28,11 @@ def course_detail(request, name):
     if request.method == 'POST':
         form = CourseForm(request.POST, request.FILES)
         if form.is_valid():
+            course_form = form.save(commit=False)
+            course_form.author = request.user.username
+            course_form.category = Catogaries.objects.get(name=name)
+            slug = re.sub(r'[^\w\s]', '', form.cleaned_data['title']).lower()
+            course_form.slug = re.sub(r'\s+', '_', slug)
             form.save()
     else:
         form = CourseForm()
@@ -37,6 +44,8 @@ def course_detail(request, name):
     if request.user.user_role == 'STUDENT':
         return render(request, 'courses/filtered_course.html', context)
     else:
+        filtered_courses = filtered_courses.filter(author=request.user.username)
+        context['filtered_courses'] = filtered_courses
         return render(request, 'courses/M_filtered.html', context)
 
 
@@ -44,7 +53,8 @@ def filtered_content(request, name, slug):
     courses = Course.objects.filter(slug=slug)
 
     if request.method == 'POST':
-        form = CourseForm(request.POST, request.FILES, instance=courses.first())  # Pass the instance to the form for updating
+        form = CourseForm(request.POST, request.FILES,
+                          instance=courses.first())  # Pass the instance to the form for updating
         if form.is_valid():
             form.save()
     else:
@@ -60,8 +70,8 @@ def filtered_content(request, name, slug):
         return render(request, 'courses/course_content.html', context)
     else:
         return render(request, 'courses/M_course.html', context)
-    
 
-#membership pages
+
+# membership pages
 def plan_details(request):
     return render(request, 'website_content/plans.html')
